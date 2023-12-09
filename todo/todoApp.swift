dc35cc46730694b13919c663bdb31df810eb837a
Varun Shembekar <varun247shembekar@gmail.com>
2023-12-09 01:35:02 -0500

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Varun Shembekar on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
